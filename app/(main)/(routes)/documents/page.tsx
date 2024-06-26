a06131bad1c571b90bfed8f7eed30bbfa06f7282
Divya Priyadarshini <divyapriyadarshini193@gmail.com>
2024-06-26 15:33:37 +0530

    “deploy”

---
+++ app/(main)/(routes)/documents/page.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import Image from "next/image";
+import { useUser } from "@clerk/clerk-react";
+import { PlusCircle } from "lucide-react";
+import { toast } from "sonner";
+
+import { Button } from "@/components/ui/button";
+import { useMutation } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import { useRouter } from "next/navigation";
+
+const DocumentsPage = () => {
+    const router = useRouter();
+    const { user }= useUser();
+    const create =  useMutation(api.documents.create);
+
+    const onCreate = () =>{
+        const promise = create({ title:"Untitled"}) 
+            .then((documentId) => router.push)
+
+
+        toast.promise(promise, {
+            loading:"Creating a new note...",
+            success:"New note created!",
+            error: "Failed to create a new note :("
+
+        });
+    }
+
+    return (
+        <div className="h-full flex flex-col items-center justify-center space-y-4">
+            <Image
+            src="/empty.png"
+            height="300"
+            width="300"
+            alt="Empty"
+            className="dark:hidden"
+            />
+
+        <Image
+            src="/empty-dark.png"
+            height="300"
+            width="300"
+            alt="Empty"
+            className="hidden dark:block"
+            />
+
+        <h2 className="text-lg font-medium">
+        Welcome to {user?.firstName}&apos;s Dotion
+        </h2>
+
+        <Button onClick={onCreate}>
+        <PlusCircle className="w-4 h-4 mr-2" />
+        Create a note
+      </Button>
+        </div>
+    );
+}
+
+export default DocumentsPage;
---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+    providers: [
+      {
+        domain: "https://native-terrier-37.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
